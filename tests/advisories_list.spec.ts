81044b5e7fee70b067b3f60c90594f8686e647c4
Jason-OXD <132406061+Jason-OXD@users.noreply.github.com>
2024-10-28 10:22:32 -0700

    Updated advisories_list to match styling change

--- tests/advisories_list.spec.ts
+++ tests/advisories_list.spec.ts
@@ -4,6 +4,7 @@ test('test', async ({ page }) => {
   await page.goto(`/advisories`);
   await expect(page.locator('h1')).toContainText('Advisories');
   await expect(page.locator('#root')).toContainText('Get the latest critical info impacting travel on a highway or region.');
-  await expect(page.locator('#root')).toContainText('REMINDER: This is a beta version of DriveBC.');
+  await expect(page.locator('p').filter({ hasText: 'This is a beta version of' }).locator('svg')).toBeVisible();
+  await expect(page.locator('#root')).toContainText('This is a beta version of DriveBC.');
   await expect(page.locator('#root')).toContainText('Advisories shared on the beta version are for feedback purposes and possibly not up-to-date. Please continue to refer to the Ministry\'s TranBC site under Current Road Advisories and Information.');
 });
